36ae39c3ce8f3b557521d2fc8dad8650634ac80c
q_osc <q_osc@LAPTOP-SCGGG79M>
2023-01-26 19:03:18 +0000

    Add project files.

---
+++ RestaurantApp/RestaurantApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RestaurantApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Task.Run(RotateImage); 
+        }
+
+        //Rotates the image
+        private async void RotateImage()
+        {
+            while (true)
+            {      
+                                    
+                await BannerImg.RelRotateTo(360, 5000, Easing.Linear);
+            }
+        }
+    }
+}
---
+++ RestaurantApp/RestaurantApp/OrderPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RestaurantApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OrderPage : ContentPage
+    {
+        public OrderPage()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void CarouselPositionChanged(object sender, PositionChangedEventArgs e)
+        {
+            var carousel = sender as CarouselView; //Object carousel is a CarouselView component
+            var views = carousel.VisibleViews; //Provides list of all views?
+
+            if (views.Count > 0)
+            {
+                foreach (var view in views)
+                {
+                    var img = view.FindByName<Image>("MenuImg"); //Finding the name of images via the x:name in XAML script
+                    ViewExtensions.CancelAnimations(img);
+
+                    //Another rotate command, not different animation type using Easing
+                    Task.Run(async () => await img.RelRotateTo(360, 5000, Easing.BounceOut));
+                }
+            } 
+        }
+        private async void BackCommand()
+        {
+            await Navigation.PopAsync();
+        }
+
+    }
+
+
+}
